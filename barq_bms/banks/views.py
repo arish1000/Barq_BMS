d84e2d71d470305a765a6ef0fb55c5b929b151d8
Arish <arishm1000@gmail.com>
2025-08-01 18:20:58 +0500

    install drf and impliment is api_view

--- barq_bms/banks/views.py
+++ barq_bms/banks/views.py
@@ -1,9 +1,23 @@
 from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
-from django.contrib.auth.decorators import login_required
+from rest_framework import status
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 from .models import Bank
+from .serializers import BankSerializer
 
-@login_required
-def bank_view(request):
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def bank_list_view(request):
     banks = Bank.objects.all().values('id', 'name', 'is_islamic')
-    return JsonResponse(list(banks), safe=False)
+    return Response(list(banks))
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def bank_detail_view(request, id):
+    try:
+        bank = Bank.objects.get(pk=id)
+        serializer = BankSerializer(bank)
+        return Response(serializer.data)
+    except Bank.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
